2d8332f714def479590494be0094cf73107a4c61
Weizhau <59955383+Weizhau@users.noreply.github.com>
2020-03-28 22:17:56 +0300

    Add files via upload

--- src/main/kotlin/subtask2/MiniMaxSum.kt
+++ src/main/kotlin/subtask2/MiniMaxSum.kt
@@ -4,6 +4,21 @@ class MiniMaxSum {
 
     // TODO: Complete the following function
     fun getResult(input: IntArray): IntArray {
-        throw NotImplementedError("Not implemented")
+        var minSum: Int = 0
+        var maxSum: Int = 0
+        var output = IntArray(2)
+
+        input.sort()
+        for (i in 0..input.size - 2) {
+            minSum += input[i]
+        }
+        for (i in input.size - 1 downTo 1) {
+            maxSum += input[i]
+        }
+
+        output[0] = minSum
+        output[1] = maxSum
+
+        return output
     }
 }

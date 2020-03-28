e448478515dba453b497cd8163e121fd6d243581
Weizhau <59955383+Weizhau@users.noreply.github.com>
2020-03-28 22:16:19 +0300

    Add files via upload

--- src/main/kotlin/subtask1/HappyArray.kt
+++ src/main/kotlin/subtask1/HappyArray.kt
@@ -2,8 +2,23 @@ package subtask1
 
 class HappyArray {
 
-    // TODO: Complete the following function
     fun convertToHappy(sadArray: IntArray): IntArray {
-        throw NotImplementedError("Not implemented")
+        var output = ArrayList(sadArray.toList())
+        var isHappy = false
+
+        while (!isHappy) {
+            isHappy = true
+            if (output.size > 2) {
+                var i = 1
+                while (i < output.size - 1) {
+                    if (output[i - 1] + output[i + 1] < output[i]) {
+                        output.removeAt(i)
+                        isHappy = false
+                    }
+                    i++
+                }
+            }
+        }
+        return output.toIntArray()
     }
 }
